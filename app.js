9b5fbc6f7598c7318fd50687b3d6d4ec44d74258
brandart <31988348+brandart@users.noreply.github.com>
2017-11-29 19:47:28 +0100

    Final solution
    Here is my solution for the Pokemon homework,
    
    I tried my best and I am totally happy with my solution. The only thing I can criticise is, that in the browser it looks booring.
    
    Best regards,
    Arthur Brandstetter

--- app.js
+++ app.js
@@ -1,16 +1,57 @@
-$(document).ready(function(){
-    const url = 'https://pokeapi.co/api/v2/pokemon/';
-    listPokemons(url);
-});  
+(async function() {
+    let pokelist = await $.get('https://pokeapi.co/api/v2/pokemon/');
 
-function listPokemons(url){
-    (async function (){
-        const pokelist = await $.get(url);
+    loadPokemons();
 
+    function loadPokemons(){
         let html = '';
-        for(const pokemon of pokelist.results){
-            html += `<tr><td> ${pokemon.name} </td><td>Button</td></tr>`;
+        for(const pokemon of pokelist.results) {
+            html += `<li>${pokemon.name} `;
+            html += '<button value = "' + pokemon.name + '" class="showDetails" >details</button>';
+            html += ' <button value = "' + pokemon.name + '" class="hideDetails">hide details</button>';
+            html += '<div id = "' + pokemon.name + '"></div>'
+            html += ' </li><br />';
         }
-        $('#resultBody')[0].innerHTML = html;
-    })();
-}
+
+        $('#pokemons')[0].innerHTML = html;
+    }
+    
+
+    $('.navigation').click(
+      async function()
+      {
+          if(this.id === "next"){
+            pokelist = await $.get(pokelist.next);
+          }else{
+              if(pokelist.previous !== null){
+                  pokelist = await $.get(pokelist.previous);
+              }
+            
+          }
+        
+        loadPokemons();
+      }  
+    );
+    $(".showDetails").click(
+        async function()
+        {
+            const pokemon = await $.get('https://pokeapi.co/api/v2/pokemon/' + $(this).val());
+            let html = 'Name: ' + pokemon.forms[0].name + '<br/>';
+            html += '<img src="' + pokemon.sprites.front_default + '"/>';
+            html += '<br/>Abilities: <br/>';
+            for(const abilitiesPoke of pokemon.abilities){
+                html += abilitiesPoke.ability.name + '<br/>';
+            }
+            html += 'Weight: ' + pokemon.weight;
+            $("#"+$(this).val())[0].innerHTML = html;
+        }
+    );
+
+    $(".hideDetails").click(
+        function()
+        {
+            const html = ``;
+            $("#"+$(this).val())[0].innerHTML = html;
+        }
+    );
+})();
